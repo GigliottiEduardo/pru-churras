26fa6c4dcc68b90d869c1701c25cd80e75805d2f
Eduardo Gigliotti <eduardosilvagigliotti@hotmail.com>
2023-03-05 00:37:25 -0300

    acabando projeto

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -8,6 +7,66 @@ import { ActionSheetController } from '@ionic/angular';
 })
 export class Tab2Page {
 
+  contando=1;
+  pctimeum=0;
+  pctimedois=0;
+
   constructor() {}
+
+  somarum () {
+    this.contando = 1
+  }
+
+  somartres () {
+    this.contando = 3
+  }
+
+  somarseis () {
+    this.contando = 6
+  }
+
+  somarnove () {
+    this.contando = 9
+  }
+
+  somardoze () {
+    this.contando = 12
+  }
+
+  botaosomatimeum () {
+    this.pctimeum += this.contando
+    this.contando = 1
+  }
+
+  botaosomatimedois () {
+    this.pctimedois += this.contando
+    this.contando = 1
+  }
+
+  botaosubtraitimeum () {
+    this.pctimeum -= this.contando
+    this.contando = 1
+
+    if (this.pctimeum <= 0) {
+      this.pctimeum = 0
+    }
+  }
+
+  botaosubtraitimedois () {
+    this.pctimedois -= this.contando
+    this.contando = 1
+
+    if (this.pctimedois <= 0) {
+      this.pctimedois = 0
+    }
+  }
+
+  PlacarTime1() {
+ 
+  }
+
+  PlacarTime2 () {
+    
+  }
 
 }
